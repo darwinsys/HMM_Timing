76e98689fe96af494b70ae7360770cb1293701e4
cocojumbo77 <jianboxue@me.com>
2015-10-09 19:50:16 +0800

    v1.1

--- test_20151009.R
+++ test_20151009.R
@@ -7,11 +7,8 @@ source("gmmhmm.R")
 load_data <- function()
 {
   data <- read.csv("data/NAV_5ETFs.csv")
-  zz500 <- as.xts(data[, 2], order.by=strptime(data[, 1], format="%Y-%m-%d", tz=""))
-  zz500 <- na.omit(cbind(zz500, Return.calculate(zz500)))
   benchmark <- as.xts(data[, 2:6], order.by=strptime(data[,1], format="%Y-%m-%d", tz=""))
   benchmark_ret <- na.omit(Return.calculate(benchmark, method = "discrete"))
-  colnames(zz500) <- c('zz500', 'zz500.ret')
   
   d <- list();
   d[[1]] <- benchmark;
@@ -30,10 +27,72 @@ test_oot1 <- function() {
 }
 
 test_oot2 <- function() {
+  ret_zz500 <- benchmark_ret[, 1]
+  ret_hs300 <- benchmark_ret[, 2]
+  ret_hengsheng <- benchmark_ret[, 5]
+  
   ema2 <- TTR::EMA(benchmark[,1], 2);
-  data_oot <- cbind.xts(benchmark_ret[, c(1, 2, 5)],  Return.calculate(ema2), lag(benchmark_ret[,1], 1), lag(benchmark_ret[, 1], 5));
+  ema5 <- TTR::EMA(benchmark[,1], 5);
+  data_oot <- cbind.xts(ret_zz500, ret_zz500 - ret_hs300, ret_zz500 - ret_hengsheng,
+              Return.calculate(ema2),  Return.calculate(ema5),
+   #           Return.calculate(TTR::EMA(benchmark[, 2]), 2),
+              lag(ret_zz500, 1), lag(ret_zz500, 5));
   data_oot <- na.omit(data_oot)
   ret <- gmmhmm(dataset = data_oot, ret_target = benchmark_ret[, 1], n_start = 1000, n_state = 5)
+  write.csv(as.data.frame(ret), 'test_results/oot2.csv')
+}
+
+test_oot3 <- function() {
+  ret_zz500 <- benchmark_ret[, 1]
+  ret_hs300 <- benchmark_ret[, 2]
+  ret_hengsheng <- benchmark_ret[, 5]
+  
+  ret_zz500_ema2 <- Return.calculate(TTR::EMA(benchmark[,1], 2))
+  ret_zz500_ema5 <- Return.calculate(TTR::EMA(benchmark[,1], 5))
+  ret_hs300_ema2 <- Return.calculate(TTR::EMA(benchmark[,2], 2))
+  ret_hs300_ema5 <- Return.calculate(TTR::EMA(benchmark[,2], 5))
+  
+  
+  data_oot3 <- cbind.xts(ret_zz500, ret_zz500 - ret_hs300, ret_zz500 - ret_hengsheng,
+                        ret_zz500_ema2, ret_zz500_ema5,
+                        ret_zz500_ema2 - ret_hs300_ema2,
+                        #           Return.calculate(TTR::EMA(benchmark[, 2]), 2),
+                        lag(ret_zz500, 1), lag(ret_zz500, 5));  
+  data_oot3 <- na.omit(data_oot3)
+  data_oot3 <- data_oot3[500:1596, ]
+  ret_oot3 <- gmmhmm(dataset = data_oot3, ret_target = data_oot3[, 1], n_start = 1000, n_state = 5)
+  rbind(table.AnnualizedReturns(ret_oot3), SharpeRatio(ret_oot3), maxDrawdown(ret_oot3))
+  write.csv(as.data.frame(ret), 'test_results/oot3.csv')
+}
+
+## 使用最新的数据（截止到2015年10月的
+test_oot4 <- function() {
+  data <- read.csv("data/Index_3.csv")
+  benchmark <- as.xts(data[, 2:4], order.by=strptime(data[,1], format="%Y/%m/%d", tz=""))
+  benchmark_w <- benchmark[endpoints(benchmark, on = "weeks")]
+  benchmark_ret <- na.omit(Return.calculate(benchmark, method = "discrete"))
+  benchmark_ret_w <- na.omit(Return.calculate(benchmark_w, method="discrete"))
+  
+  ret_zz500 <- benchmark_ret[, 1]
+  ret_hs300 <- benchmark_ret[, 2]
+  ret_hengsheng <- benchmark_ret[, 3]
+  
+  ret_zz500_ema2 <- Return.calculate(TTR::EMA(benchmark[,1], 2))
+  ret_zz500_ema5 <- Return.calculate(TTR::EMA(benchmark[,1], 5))
+  ret_hs300_ema2 <- Return.calculate(TTR::EMA(benchmark[,2], 2))
+  ret_hs300_ema5 <- Return.calculate(TTR::EMA(benchmark[,2], 5))
+  
+  
+  data_oot4 <- cbind.xts(ret_zz500, ret_zz500 - ret_hs300, #ret_zz500 - ret_hengsheng,
+                         ret_zz500_ema2, ret_zz500_ema5,
+                         #           Return.calculate(TTR::EMA(benchmark[, 2]), 2),
+                         lag(ret_zz500, 1), lag(ret_zz500, 5));  
+  data_oot4 <- na.omit(data_oot4)
+  ret_oot4 <- gmmhmm(dataset = data_oot4, ret_target = data_oot4[, 1], n_start = 1000, n_state = 5)
+  rbind(table.AnnualizedReturns(ret_oot4), SharpeRatio(ret_oot4), maxDrawdown(ret_oot4))
+  write.csv(as.data.frame(ret), 'test_results/oot4.csv')
+  
+  
 }
 
 
